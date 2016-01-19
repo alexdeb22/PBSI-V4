1470ee1a6f277f7f66ef6d57d21ee38d8da4e989
clempeb <clement.peberge@hotmail.fr>
2016-01-19 12:15:45 +0100

    mockito ok

--- pbsiV4/Service/src/main/java/fr/gtm/proxibanquesi/service/ServiceClient.java
+++ pbsiV4/Service/src/main/java/fr/gtm/proxibanquesi/service/ServiceClient.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.gtm.proxibanquesi.dao.IDaoClient;
 import fr.gtm.proxibanquesi.domaine.Client;
 
 /**
--- pbsiV4/Service/src/test/java/fr/gtm/proxibanquesi/test/TestServiceClient.java
+++ pbsiV4/Service/src/test/java/fr/gtm/proxibanquesi/test/TestServiceClient.java
@@ -7,20 +7,30 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.verification.VerificationMode;
+
+import fr.gtm.proxibanquesi.dao.IDaoClient;
+import fr.gtm.proxibanquesi.service.ServiceClient;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestServiceClient {
 	@Mock
 	IDaoClient dao;
 	
+	ServiceClient ser;
+	VerificationMode vf;
 
 	@Before
 	public void setUp(){
 		MockitoAnnotations.initMocks(this);
+		ser = new ServiceClient();
+		ser.setDao(dao);
 	}
 	
 	@Test
 	public void testCallFindAll(){
+		ser.findAll();
 		Mockito.verify(dao).findAll();
+		
 	}
 }
