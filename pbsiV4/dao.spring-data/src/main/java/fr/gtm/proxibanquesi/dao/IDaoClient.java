73db218e85c6d98b7446f58033d8b6879753ca2d
clempeb <clement.peberge@hotmail.fr>
2016-01-19 16:46:34 +0100

    context-service

--- pbsiV4/dao.spring-data/src/main/java/fr/gtm/proxibanquesi/dao/IDaoClient.java
+++ pbsiV4/dao.spring-data/src/main/java/fr/gtm/proxibanquesi/dao/IDaoClient.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import fr.gtm.proxibanquesi.domaine.Client;
 
+/**
+ * Interface DaoClient. Elle liste les méthodes CRUD d'un client et une méthode qui retourne la liste de tous les clients
+ *
+ */
 @Repository("dao")
 public interface IDaoClient extends JpaRepository<Client, Integer> {
 
