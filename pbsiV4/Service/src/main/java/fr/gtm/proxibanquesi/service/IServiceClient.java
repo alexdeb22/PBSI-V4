ccdf8845b3b61a7392c13eb90dc2a6d1962ad056
Clelou <clelia.betton@laposte.net>
2016-01-19 10:10:52 +0100

    Service import

--- pbsiV4/Service/src/main/java/fr/gtm/proxibanquesi/service/IServiceClient.java
+++ pbsiV4/Service/src/main/java/fr/gtm/proxibanquesi/service/IServiceClient.java
@@ -2,6 +2,8 @@ package fr.gtm.proxibanquesi.service;
 
 import java.util.List;
 
+import fr.gtm.proxibanquesi.domaine.Client;
+
 public interface IServiceClient {
 	
 	public List<Client> findAll();
--- pbsiV4/Service/src/main/java/fr/gtm/proxibanquesi/service/ServiceClient.java
+++ pbsiV4/Service/src/main/java/fr/gtm/proxibanquesi/service/ServiceClient.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.gtm.proxibanquesi.domaine.Client;
+
 @Service("serviceClient")
 public class ServiceClient implements IServiceClient {
 	
