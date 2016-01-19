e36d6ae1233792b82470bb08bc5fb1aa516a34ec
Clelou <clelia.betton@laposte.net>
2016-01-19 10:58:43 +0100

    commit2

--- pbsiV4/Service/src/test/java/fr/gtm/proxibanquesi/test/TestServiceClient.java
+++ pbsiV4/Service/src/test/java/fr/gtm/proxibanquesi/test/TestServiceClient.java
@@ -1,17 +1,26 @@
 package fr.gtm.proxibanquesi.test;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestServiceClient {
+	@Mock
+	IDaoClient dao;
+	
 
 	@Before
 	public void setUp(){
 		MockitoAnnotations.initMocks(this);
 	}
 	
-	
+	@Test
+	public void testCallFindAll(){
+		Mockito.verify(dao).findAll();
+	}
 }
