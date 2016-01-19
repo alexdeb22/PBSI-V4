9f0e6b7e2ef77201cb978f143a072bd46447cf45
Clelou <clelia.betton@laposte.net>
2016-01-19 10:36:15 +0100

    Mockito

--- pbsiV4/Service/src/main/java/fr/gtm/proxibanquesi/service/IServiceClient.java
+++ pbsiV4/Service/src/main/java/fr/gtm/proxibanquesi/service/IServiceClient.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 import fr.gtm.proxibanquesi.domaine.Client;
 
+/**
+ * Interface ServiceClient. Elle liste les méthodes CRUD d'un client et une méthode qui retourne la liste de tous les clients
+ *
+ */
 public interface IServiceClient {
 	
+	/**
+	 * Methode qui retourne la liste de tous les clients
+	 */
 	public List<Client> findAll();
 
 }
