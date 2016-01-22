1da2befdc534753b3e9c1721ce55a7557fb6f7b6
Clelou <clelia.betton@laposte.net>
2016-01-22 14:31:19 +0100

    Dernière javadoc

--- pbsiV4/front/src/main/java/fr/gtm/proxibanquesi/front/beans/GraphicBean.java
+++ pbsiV4/front/src/main/java/fr/gtm/proxibanquesi/front/beans/GraphicBean.java
@@ -2,8 +2,6 @@ package fr.gtm.proxibanquesi.front.beans;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
-
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 
@@ -25,25 +23,36 @@ import fr.gtm.proxibanquesi.domaine.Transaction;
 @Component
 public class GraphicBean implements Serializable{
 
-	/**
-	 * 
-	 */
+	/** Clé identifie de manière unique la Classe */
 	private static final long serialVersionUID = 1L;
+	/** Classe lineChartModel */
 	private LineChartModel dateModel;
 	
+	/**
+	 * Interface IDaoTransaction injecté par Spring
+	 */
 	@Autowired
 	IDaoTransaction transacDao;
 	
+	/**
+	 * Constructeur de GraphicBean sans argument
+	 */
 	public GraphicBean() {
 		super();
 	}
 	
+	/**
+	 * Methode appelée avant l'initialisation du bean 
+	 */
 	@PostConstruct
     public void init() {
         createDateModel();
         System.out.println("creation bean graph");
     }
 	
+	/**
+	 * Méthode permettant  modèle de données
+	 */
 	private void createDateModel() {
 		System.out.println("create data model");
         dateModel = new LineChartModel();
@@ -70,10 +79,19 @@ public class GraphicBean implements Serializable{
           
     }
 	
+	/**
+	 * getter de l'attribut dateModel
+	 * @return l'instance selectedClient
+	 */
 	public LineChartModel getDateModel() {
 		System.out.println("get data model");
 		return dateModel;
 	}
+	
+	/**
+	 * setter de l'attribut dateModel
+	 * @param dateModel
+	 */
 	public void setDateModel(LineChartModel dateModel) {
 		this.dateModel = dateModel;
 	}
